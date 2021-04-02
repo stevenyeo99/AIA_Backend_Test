efd442672cec037f2ae8fbeed316ecd9d7a7d902
steven.yeo <steven.yeo@velocity-solutions.com>
2021-04-02 13:45:57 +0700

    20210402 Initialize Springboot Project Startup

---
+++ src/main/java/com/hubbers/aia/steven/backend/test/AiaBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.hubbers.aia.steven.backend.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiaBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiaBackendTestApplication.class, args);
+	}
+
+}
